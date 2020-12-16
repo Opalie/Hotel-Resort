0cb11f3e3c2440f71fb8ef2d9a4f782243006102
clement-dsvs <72504071+clement-dsvs@users.noreply.github.com>
2020-12-16 16:13:25 +0100

    Add files via upload

---
+++ Hotel-Resort-main/script.js
@@ -0,0 +1,35 @@
+$('document').ready(function() {
+    $('.status').hide(200);
+    let cache = true;
+
+    $('.deux').click(function() {
+        console.log($(this).attr('id'));
+    })
+});
+
+
+/*  SAVE========================
+
+$('document').ready(function() {
+    $('.status').hide(200);
+    let cache = true;
+
+    $('.deux').click(function() {
+        if (cache === true){
+            $('.status')
+                .fadeIn(300)
+                .css("z-index", "0")
+            $('.deux').css("z-index", "1")
+            cache = false;
+        }
+        else {
+            $('.status')
+                .fadeOut(300)
+                .css("Z-Index", "1");
+            $('.deux').css("z-index", "0")
+            cache = true;
+        }
+    })
+});
+
+*/
