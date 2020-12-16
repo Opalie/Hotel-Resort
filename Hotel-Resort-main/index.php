bb353e3311db31ad5736b810fb41310696482518
Opalie <31365045+Opalie@users.noreply.github.com>
2020-12-16 14:40:56 +0100

    Added Login CSS
    + changed Index to add a parent div

--- Hotel-Resort-main/index.php
+++ Hotel-Resort-main/index.php
@@ -1,11 +1,12 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="style/css-login.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel F2</title>
 </head>
 <body>
-
+<div class="coolheight">
 <div class="container-login">
     <form action="includes/login.inc.php" method="post">
         <label for="login">Login</label>
@@ -15,6 +16,6 @@
         <input type="submit" name="submit">
     </form>
 </div>
-    
+</div>
 </body>
 </html>
