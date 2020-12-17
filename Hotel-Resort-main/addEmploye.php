bc4265cbc1b8fabdd0e34a6cb02b78d490c1cf3c
Opalie <31365045+Opalie@users.noreply.github.com>
2020-12-17 12:08:52 +0100

    added css to addEmploye

--- Hotel-Resort-main/addEmploye.php
+++ Hotel-Resort-main/addEmploye.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/css-manager.css">
+    <link rel="stylesheet" href="style/css-AddEmploye.css">
     <title>Hotel Ressort F2</title>
 </head>
 
