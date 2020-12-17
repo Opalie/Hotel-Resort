5834a63f8706562dd00f5f2cac9bd7a26c10c2dd
Opalie <31365045+Opalie@users.noreply.github.com>
2020-12-17 13:33:07 +0100

    fixed Index.php

--- Hotel-Resort-main/index.php
+++ Hotel-Resort-main/index.php
@@ -2,10 +2,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/css-login.css">
     <title>Hotel F2</title>
 </head>
 <body>
-
+<div class="coolheight">
 <div class="container-login">
     <form action="includes/login.inc.php" method="post">
         <label for="login">Login</label>
@@ -15,6 +16,6 @@
         <input type="submit" name="submit">
     </form>
 </div>
-    
+</div>
 </body>
 </html>
